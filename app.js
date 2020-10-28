28ccddffd6af9791a8e34e5b0646454c11267fd8
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-10-28 14:11:50 -0300

    Adicionando os arquivos iniciais do projeto

---
+++ app.js
@@ -0,0 +1,26 @@
+const puppeteer = require('puppeteer')
+
+// Título do último vídeo postado no canal
+var last_video
+
+// Entrando no canal e pegando o nome do vídeo
+async function startApp(){
+    function getTitle(page, selector){
+        var title = page.$eval(selector, title => title.innerText)
+        return title
+    }
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+
+    await page.goto('https://www.youtube.com/c/DiegoFaustino68/videos')
+    last_video = await getTitle(page, '#video-title')
+    
+    browser.close()
+    
+    console.log(last_video)
+}
+
+//Chamando função
+startApp();
+setInterval(() => startApp(), 5000);
