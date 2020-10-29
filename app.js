5f5a10459d8ba1f4ce14822646da4eb915b4d4d2
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-10-29 15:11:45 -0300

    Adicionando getLink() e sendEmail() ao app.js

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 const puppeteer = require('puppeteer')
+var nodemailer = require('nodemailer');
 
 // Título do último vídeo postado no canal
-var last_video
+var last_video = 'MINHA SILVERADO TRANSFORMOU ! FICOU LINDA DEMAIS'
 
 // Entrando no canal e pegando o nome do vídeo
 async function startApp(){
@@ -10,18 +11,60 @@ async function startApp(){
         return title
     }
 
+    function getLink(page, selector){
+        var link = page.$eval(selector, link => link.href)
+        return link
+    }
+
+    function sendEmail(link_video){
+        var sender = nodemailer.createTransport({
+            host: 'smtp.gmail.com',
+            service: 'gmail',
+            port: 587,
+            secure: true,
+            auth: {
+                user: 'capitaocaverna012@gmail.com',
+                pass: 'qualquercoisa'
+            }
+        });
+
+        var emailToSend = {
+            from: 'jose.vitor.clashbr@gmail.com',
+            to: 'jose.vitor.clashbr@gmail.com',
+            subject: 'Vídeo novo do Diego Faustino',
+            text: 'Esse é o link para o novo vídeo do Diego Faustino: \n' + link_video
+        };
+
+        sender.sendMail(emailToSend, function (error) {
+            if (error) {
+                console.log(error);
+            } else {
+                console.log('Email enviado com sucesso!');
+            }
+        });
+    }
+
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
     await page.goto('https://www.youtube.com/c/DiegoFaustino68/videos')
-    last_video = await getTitle(page, '#video-title')
+
+    var new_video = await getTitle(page, '#video-title')
     
-    browser.close()
-    
-    console.log(last_video)
+    if(new_video == last_video){
+        browser.close()
+        console.log("Sem vídeo novo!")    
+    } else{
+        var link = await getLink(page, '#video-title') 
+        sendEmail(link)
+        last_video = new_video
+        browser.close()
+        console.log('Video novo: ' + new_video)
+    }
 }
 
 //Chamando função
 startApp();
-//Function que faz a repetição do startApp()
-setInterval(() => startApp(), 5000);
+
+//Function que faz a repetição do startApp() a cada 2 minutos (120000)
+setInterval(() => startApp(), 120000);
