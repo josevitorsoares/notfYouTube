747ef2628c2fd549fb10643142d8b656d88b605d
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-10-29 23:17:31 -0300

    modularizando o codigo em funcoes separadas

--- app.js
+++ app.js
@@ -1,70 +1,72 @@
 const puppeteer = require('puppeteer')
-var nodemailer = require('nodemailer');
+const nodemailer = require('nodemailer')
 
-// Título do último vídeo postado no canal
-var last_video = 'MINHA SILVERADO TRANSFORMOU ! FICOU LINDA DEMAIS'
+var last_video = ''
 
-// Entrando no canal e pegando o nome do vídeo
-async function startApp(){
-    function getTitle(page, selector){
-        var title = page.$eval(selector, title => title.innerText)
-        return title
-    }
+//Function para obter o título do video
+function getTitle(page, selector){
+    var title = page.$eval(selector, title => title.innerText)
+    return title
+}
 
-    function getLink(page, selector){
-        var link = page.$eval(selector, link => link.href)
-        return link
-    }
+//Function para obter o link do video
+function getLink(page, selector){
+    var link = page.$eval(selector, link => link.href)
+    return link
+}
 
-    function sendEmail(link_video){
-        var sender = nodemailer.createTransport({
-            host: 'smtp.gmail.com',
-            service: 'gmail',
-            port: 587,
-            secure: true,
-            auth: {
-                user: 'capitaocaverna012@gmail.com',
-                pass: 'qualquercoisa'
-            }
-        });
+//Function para o envio do email
+function sendEmail(link_video){
+    var sender = nodemailer.createTransport({
+        host: 'smtp.gmail.com',
+        service: 'gmail',
+        port: 587,
+        secure: true,
+        auth: {
+            user: 'capitaocaverna012@gmail.com',
+            pass: 'qualquercoisa'
+        }
+    });
 
-        var emailToSend = {
-            from: 'jose.vitor.clashbr@gmail.com',
-            to: 'jose.vitor.clashbr@gmail.com',
-            subject: 'Vídeo novo do Diego Faustino',
-            text: 'Esse é o link para o novo vídeo do Diego Faustino: \n' + link_video
-        };
+    var emailToSend = {
+        from: 'jose.vitor.clashbr@gmail.com',
+        to: 'jose.vitor.clashbr@gmail.com',
+        subject: 'Vídeo novo do Diego Faustino',
+        text: 'Esse é o link para o novo vídeo do Diego Faustino: \n' + link_video
+    };
 
-        sender.sendMail(emailToSend, function (error) {
-            if (error) {
-                console.log(error);
-            } else {
-                console.log('Email enviado com sucesso!');
-            }
-        });
-    }
+    sender.sendMail(emailToSend, function (error) {
+        if (error) {
+            console.log(error);
+        } else {
+            console.log('Email enviado com sucesso!');
+        }
+    });
+}
 
+// Function principal
+async function startAppJS(){
     const browser = await puppeteer.launch()
     const page = await browser.newPage()
 
     await page.goto('https://www.youtube.com/c/DiegoFaustino68/videos')
 
     var new_video = await getTitle(page, '#video-title')
-    
-    if(new_video == last_video){
-        browser.close()
-        console.log("Sem vídeo novo!")    
-    } else{
-        var link = await getLink(page, '#video-title') 
-        sendEmail(link)
-        last_video = new_video
-        browser.close()
-        console.log('Video novo: ' + new_video)
-    }
+
+    // if(new_video != last_video){
+    //     var link = await getLink(page, '#video-title') 
+    //     sendEmail(link)
+    //     last_video = new_video
+    //     console.log('Video novo: ' + new_video)      
+    // } else if(new_video == last_video){
+    //     console.log("Sem vídeo novo!")
+    // }
+
+    browser.close()
 }
 
-//Chamando função
-startApp();
+//Iniciando a function principal
+startAppJS();
 
-//Function que faz a repetição do startApp() a cada 2 minutos (120000)
-setInterval(() => startApp(), 120000);
+//Function que faz a repetição da execução do startApp() a cada 2 minutos (120000)
+//setInterval(() => startApp(), 120000);
