4a47deb90fde7b6e442cc3bac769963d4d0a7d0e
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-11-03 18:48:26 -0300

    Testando tutorial StackOverFlow

--- app.js
+++ app.js
@@ -47,7 +47,12 @@ function sendEmail(link_video) {
 
 // Function principal que executa o que é proposto pelo robô
 async function startAppJS() {
-    const browser = await puppeteer.launch()
+    const browser = await puppeteer.launch({
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+        ],
+      });
     const page = await browser.newPage()
 
     await page.goto('https://www.youtube.com/c/DiegoFaustino68/videos')
