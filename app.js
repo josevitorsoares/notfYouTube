ece249dd216d8c963e278f7c6690815a9ad15570
Jose Vitor Soares <jose.vitor.clashbr@gmail.com>
2020-11-03 21:50:16 -0300

    Testando mudança na const browser...

--- app.js
+++ app.js
@@ -47,13 +47,13 @@ function sendEmail(link_video) {
 
 // Function principal que executa o que é proposto pelo robô
 async function startAppJS() {
-    const browser = await puppeteer.launch({
-        'args': [
-          '--no-sandbox',
-          '--disable-setuid-sandbox',
-        ],
-      });
-    //const browser = await puppeteer.launch()
+    // const browser = await puppeteer.launch({
+    //     'args': [
+    //       '--no-sandbox',
+    //       '--disable-setuid-sandbox',
+    //     ],
+    //   });
+    const browser = await puppeteer.launch()
     const page = await browser.newPage()
     
     await page.goto('https://www.youtube.com/c/DiegoFaustino68/videos')
